4314d18b926dbeb0dc8218104d8bfd8b88f1cc12
Sergey Ivanov <mister.happyend@yandex.ru>
2020-03-29 10:08:24 +0300

    bugfix circle load on login

--- 2. установка NEXT-CLOUD.sh
+++ 2. установка NEXT-CLOUD.sh
@@ -1,33 +1,39 @@
 ### https://www.galkov.pro/nextcloud_on_docker/
 ### создаем папку для хранения параметров развертывания контейнеров
-mkdir /etc/docker/nc
+mkdir /app
+
 
 ### создаем файл с параметрами развертывания контейнеров /etc/docker/nc/docker-compose.yml
 ### см. docker-compose.yml
-
-#sudo mkdir /var/www
-#sudo mkdir /var/www/html
-#sudo mkdir /var/www/html/data
-#sudo chmod 777 /var/www
-#
-#sudo mkdir /var/lib/postgresql
-#sudo mkdir /var/lib/postgresql/data
-#sudo chmod 777 /var/lib/postgresql
-#
-#### SQL команды
-#create database nextcloud;
-#CREATE USER "'nextcloud'@'%'" WITH PASSWORD '#TODOPASS' ;
-#GRANT ALL PRIVILEGES ON DATABASE nextcloud TO "'nextcloud'@'%'";
-
 ### ЗАПУСК
 sudo docker-compose -f /app/docker-compose.yml up -d
 ### ИЛИ
 cd /app
 sudo docker-compose -f up -d
 
+# !!!!!!!!!!!
+#TODO ВАЖНО посмотри после развертки контейнера.. конфиг: {nextcloud}/config/config.php
+#TODO Если этого не сделать, то приложение спрятанное за https прокси будет генерить невалидные ссылки
+#баг был описан здесь: https://github.com/nextcloud/server/issues/13713
+
+    // (стоковые настройки удалены отсюда)
+    // ОЧЕНЬ ВАЖНО ДОБАВИТЬ В {nextcloud}/config/config.php
+    // Эти настройки:
+    'overwritehost' => 'bati4eli.ru',
+    'overwriteprotocol' => 'https',
+    'overwrite.cli.url' => 'https://bati4eli.ru',
+    'trusted_domains' =>
+    array (
+      0 => 'bati4eli.ru',
+      1 => '192.168.1.75',
+    ),
+
+## после внесения изменений перезапускаем:
+sudo docker-compose restart
+
 ### STOP
 sudo docker-compose stop
-
 ### Эта команда позволяет останавливать и удалять контейнеры и другие ресурсы, 
 ### созданные командой docker-compose up:
 sudo docker-compose down -v
+
---
+++ configs/nextcloud-config/config.php
@@ -0,0 +1,14 @@
+<?php
+$CONFIG = array (
+  // (стоковые настройки удалены отсюда)
+  // ОЧЕНЬ ВАЖНО ДОБАВИТЬ В {nextcloud}/config/config.php
+  // Эти настройки:
+  'overwritehost' => 'bati4eli.ru',
+  'overwriteprotocol' => 'https',
+  'overwrite.cli.url' => 'https://bati4eli.ru',
+  'trusted_domains' =>
+  array (
+    0 => 'bati4eli.ru',
+    1 => '192.168.1.75',
+  ),
+);
