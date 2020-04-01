71f7b52fb036f2ca5c9861c8ca176b0b916ed2da
Sergey Ivanov <mister.happyend@yandex.ru>
2020-04-01 11:24:49 +0300

    нашел команды для PREVIEW GENERATE

---
+++ configs/settigs_for_preview_generator.sh
@@ -0,0 +1,7 @@
+PATH=$PATH:/bin:/usr/bin:/usr/sbin
+SHELL=/bin/bash
+echo "START bash"
+docker exec -u www-data:www-data -it app_app_1 bash -c "php ./occ config:app:set --value=\"32 64 1024\"  previewgenerator squareSizes";
+docker exec -u www-data:www-data -it app_app_1 bash -c "php ./occ config:app:set --value=\"64 128 1024\" previewgenerator widthSizes";
+docker exec -u www-data:www-data -it app_app_1 bash -c "php ./occ config:app:set --value=\"64 256 1024\" previewgenerator heightSizes";
+echo "END bash"
