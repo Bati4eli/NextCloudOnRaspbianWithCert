b1b15068e7dd58a039d61fabae9526a9f7d874b1
bati4eli <mister.happyend@yandex.ru>
2020-04-02 22:24:53 +0300

    update

--- files-for-server/nextcloud-config/config.php
+++ files-for-server/nextcloud-config/config.php
@@ -1,14 +1,21 @@
 <?php
 $CONFIG = array (
-  // (стоковые настройки удалены отсюда)
-  // ОЧЕНЬ ВАЖНО ДОБАВИТЬ В {nextcloud}/config/config.php
-  // Эти настройки:
-  'overwritehost' => 'bati4eli.ru',
-  'overwriteprotocol' => 'https',
-  'overwrite.cli.url' => 'https://bati4eli.ru',
-  'trusted_domains' =>
-  array (
+    //Для добавления отображения превью видеофайлов
+    'enabledPreviewProviders' => array (
+      'OC\\Preview\\Movie',
+      'OC\\Preview\\Image'
+    ),
+    'preview_max_x' => 512,
+    'preview_max_y' => 512,
+    // (стоковые настройки удалены отсюда)
+    // ОЧЕНЬ ВАЖНО ДОБАВИТЬ В {nextcloud}/config/config.php
+    // Эти настройки:
+    'overwritehost' => 'bati4eli.ru',
+    'overwriteprotocol' => 'https',
+    'overwrite.cli.url' => 'https://bati4eli.ru',
+    'trusted_domains' =>
+    array (
     0 => 'bati4eli.ru',
     1 => '192.168.1.75',
-  ),
+    ),
 );
