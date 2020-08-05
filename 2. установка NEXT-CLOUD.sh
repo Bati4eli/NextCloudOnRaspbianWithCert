493739ccce8d8a96a056844d9d40065527a614f9
bati4eli <mister.happyend@yandex.ru>
2020-08-06 00:10:30 +0300

    update Dockerfile

--- 2. установка NEXT-CLOUD.sh
+++ 2. установка NEXT-CLOUD.sh
@@ -65,6 +65,7 @@ exit # - выход из терминала контейнера
 sudo chown www-data:www-data -R /mnt/hdd/ncdata/USER_NAME/files
 docker exec -u www-data -it app_app_1 bash
   php occ files:scan USER_NAME
+  php occ trashbin:cleanup --all-users
 exit
 
 
